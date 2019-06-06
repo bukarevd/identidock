31a5b96031529ff46b92be75fd214fa6e5fdb9c3
Ubuntu <ubuntu@ip-172-31-92-97.ec2.internal>
2019-06-06 08:22:28 +0000

    script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$ENV" = 'DEV' ]; then
+	echo "DEV ENV"
+	exec python "identidock.py"
+else
+	echo "PROD"
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+fi
+
